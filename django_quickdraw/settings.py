1a1fe76f61a8e5c501e03574f2194c5a8339cc81
Takumi Ishiyama <mt.stone20@gmail.com>
2018-12-20 02:44:23 +0900

    :pencil:Fix settings

--- django_quickdraw/settings.py
+++ django_quickdraw/settings.py
@@ -104,7 +104,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/1.11/topics/i18n/
 
-LANGUAGE_CODE = 'ja'
+LANGUAGE_CODE = 'ja-JP'
 
 TIME_ZONE = 'Asia/Tokyo'
 
@@ -112,7 +112,7 @@ USE_I18N = True
 
 USE_L10N = True
 
-USE_TZ = True
+USE_TZ = False
 
 
 # Static files (CSS, JavaScript, Images)
