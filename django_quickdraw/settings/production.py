b1b620bf62031982938de187fc3ccd797548215e
Takumi Ishiyama <mt.stone20@gmail.com>
2019-01-06 01:23:12 +0900

    :pencil:Fix setting

--- django_quickdraw/settings/production.py
+++ django_quickdraw/settings/production.py
@@ -2,3 +2,7 @@ from .base import *
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
+
+if not DEBUG:
+    import django_heroku
+    django_heroku.settings(locals())
