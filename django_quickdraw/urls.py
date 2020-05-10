f7750349910c014f803a44cd6939a2ed07812902
Takumi Ishiyama <mt.stone20@gmail.com>
2020-05-11 04:51:48 +0900

    fix

--- django_quickdraw/urls.py
+++ django_quickdraw/urls.py
@@ -13,12 +13,10 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import include, url, re_path
+from django.conf.urls import include, url
 from django.contrib import admin
-from django.views.generic import TemplateView
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url('', include('quickdraw.urls')),
-    re_path('.*', TemplateView.as_view(tempalte_name='base.html'))
 ]
